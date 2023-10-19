7f1ec49e7b23389cf47059d4ff927a5247e8928f
Ekaterina Verbitskaia <kajigor@gmail.com>
2023-10-19 12:56:33 +0200

    The test for Destructuring Task fixed.

--- Pattern Matching/Destructuring/test/DestructuringTaskSpec.scala
+++ Pattern Matching/Destructuring/test/DestructuringTaskSpec.scala
@@ -6,12 +6,12 @@ class DestructuringTaskSpec extends AnyFunSuite {
     assert(colorDescription(RGB(0, 0, 0, 0)) == "White")
     assert(colorDescription(RGB(1, 255, 0, 0)) == "Red")
     assert(colorDescription(RGB(7 ,0, 255, 0)) == "Green")
-    assert(colorDescription(RGB(13, 0, 255)) == "Blue")
+    assert(colorDescription(RGB(13, 0, 0, 255)) == "Blue")
     assert(colorDescription(RGB(42, 255, 255, 0)) == "Yellow")
-    assert(colorDescription(RGB(255, 255, 255)) == "Cyan")
+    assert(colorDescription(RGB(255, 0, 255, 255)) == "Cyan")
     assert(colorDescription(RGB(255, 255, 0, 255)) == "Magenta")
     assert(colorDescription(RGB(255, 255, 255, 255)) == "Black")
-    assert(colorDescription(RGB(255, 128, 17, 0)) == "RGB(128,17,0)")
-    assert(colorDescription(RGB(255, 0, 191, 255)) == "RGB(0,191,255)")
+    assert(colorDescription(RGB(255, 128, 17, 0)) == "RGB(255,128,17,0)")
+    assert(colorDescription(RGB(255, 0, 191, 255)) == "RGB(255,0,191,255)")
   }
 }
